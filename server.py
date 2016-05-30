c157166f31107e22a7b4dd680b1ccdea9013d23c
Peter Georgiev <georgiev2592@gmail.com>
2016-05-30 12:00:22 -0700

    converted tabs to spaces

--- server.py
+++ server.py
@@ -10,12 +10,12 @@ web.config.debug = False
 # render = web.template.render('templates/', base='layout')
 
 urls = (
-	'/', 'Login',
-	'/logout', 'Logout',
-	'/register', 'Register',
-	'/home', 'Home',
-	'/about', 'About',
-	'/contact', 'Contact'
+    '/', 'Login',
+    '/logout', 'Logout',
+    '/register', 'Register',
+    '/home', 'Home',
+    '/about', 'About',
+    '/contact', 'Contact'
 )
 
 app = web.application(urls, globals())
@@ -28,253 +28,262 @@ db = MySQLdb.connect(host = 'hms.cytvwijmz1je.us-west-2.rds.amazonaws.com',
 cur = db.cursor()
 
 if web.config.get('_session') is None:
-	session = web.session.Session(app, web.session.DiskStore('sessions'), initializer={'login': 0, 'privilege': -1, 'name': ''})
-	web.config._session = session
+    session = web.session.Session(app, web.session.DiskStore('sessions'), initializer={'login': 0, 'privilege': -1, 'name': ''})
+    web.config._session = session
 else:
-	session = web.config._session
+    session = web.config._session
 
 class Login:
-	login_form = form.Form(
-		form.Textbox('email',
-			form.notnull,
-			description='Email',
-			id='emailBox',
-			class_ = 'form-control',
-			placeholder='Enter your email'),
-		form.Password('password',
-			form.notnull,
-			description='Password',
-			id='passwordBox',
-			class_ = 'form-control',
-			placeholder='Enter your password'),
-		form.Button('Login',
-			id='loginButton',
-			class_ = 'btn btn-default btn-block'))
+    login_form = form.Form(
+        form.Textbox('email',
+            form.notnull,
+            description='Email',
+            id='emailBox',
+            class_ = 'form-control',
+            placeholder='Enter your email'),
+        form.Password('password',
+            form.notnull,
+            description='Password',
+            id='passwordBox',
+            class_ = 'form-control',
+            placeholder='Enter your password'),
+        form.Button('Login',
+            id='loginButton',
+            class_ = 'btn btn-default btn-block'))
 
-	nullform = form.Form()
+    nullform = form.Form()
 
-	def GET(self):
-		if logged():
-			render = create_render(session.privilege)
-			return '%s' % render.index('HMS | Login', self.nullform, '', '', 'Already logged in.')
-		else:
-			render = create_render(session.privilege)
-			return '%s' % render.index('HMS | Login', self.login_form(), '', '', '')
+    def GET(self):
+        if logged():
+            render = create_render(session.privilege)
+            return '%s' % render.index('HMS | Login', self.nullform, '', '', 'Already logged in.')
+        else:
+            render = create_render(session.privilege)
+            return '%s' % render.index('HMS | Login', self.login_form(), '', '', '')
 
-	def POST(self):
-		form = self.login_form()
+    def POST(self):
+        form = self.login_form()
 
-		if not form.validates():
-			session.login = 0
-			session.privilege = -1
-			render = create_render(session.privilege)
-			return render.index('HMS | Login', form, '', '', 'Invalid form data.')
+        if not form.validates():
+            session.login = 0
+            session.privilege = -1
+            render = create_render(session.privilege)
+            return render.index('HMS | Login', form, '', '', 'Invalid form data.')
 
-		email = dict(email = form.d.email)
-		pw = hashlib.sha1(form.d.password).hexdigest()
+        email = dict(email = form.d.email)
+        pw = hashlib.sha1(form.d.password).hexdigest()
 
-		# ident = db.select('Users', where={'email : $email'}, vars=email)
-		cur.execute('SELECT * FROM Users WHERE email=%s', (form.d.email))
-		ident = fetchOneAssoc(cur)
+        # ident = db.select('Users', where={'email : $email'}, vars=email)
+        cur.execute('SELECT * FROM Users WHERE email=%s', (form.d.email))
+        ident = fetchOneAssoc(cur)
 
-		try:
-			if pw == ident['encrypted_password']:
-				session.login = 1
-				session.privilege = ident['privilege']
-				session.name = ident['first_name'] + ' ' + ident['last_name']
-				render = create_render(session.privilege)
-				return render.home('HMS | Home', ident['first_name'] + ' ' + ident['last_name'], '', '', '')
-			else:
-				session.login = 0
-				session.privilege = -1
-				render = create_render(session.privilege)
-				return render.index('HMS | Login', form, '', '', 'Username/Password Incorrect')
-		except:
-			session.login = 0
-			session.privilege = -1
-			render = create_render(session.privilege)
-			return render.index('HMS | Login', form, '', '', 'Username/Password Incorrect')
+        try:
+            if pw == ident['encrypted_password']:
+                session.login = 1
+                session.privilege = ident['privilege']
+                session.name = ident['first_name'] + ' ' + ident['last_name']
+                render = create_render(session.privilege)
+
+                list_of_users = self._generateList()
+
+                return render.home('HMS | Home', ident['first_name'] + ' ' + ident['last_name'], '', '', '', list_of_users)
+            else:
+                session.login = 0
+                session.privilege = -1
+                render = create_render(session.privilege)
+                return render.index('HMS | Login', form, '', '', 'Username/Password Incorrect')
+        except:
+            session.login = 0
+            session.privilege = -1
+            render = create_render(session.privilege)
+            return render.index('HMS | Login', form, '', '', 'Username/Password Incorrect')
+
+    def _generateList(self):
+        cur.execute('SELECT * FROM Users')
+        list_of_users = fetchAllAssoc(cur)
+
+        print list_of_users
 
 class Register:
-	register_form = form.Form(
-		form.Textbox("first_name",
-			form.notnull,
-			description = "First Name",
-			class_ = "form-control",
-			placeholder='Enter employee first name'),
-		form.Textbox("last_name",
-			form.notnull,
-			description = "Last Name",
-			class_ = "form-control",
-			placeholder='Enter employee last name'),
-		form.Textbox("email",
-			form.notnull,
-			description = "Email",
-			class_ = "form-control",
-			placeholder='Enter employee email'),
-		form.Dropdown('privilege', 
-			[('0', 'Front Desk'), ('1', 'Cleaning Personnel')],
-			description = "Role",
-			class_ = "form-control",
-			placeholder='Select employee work position'),
-		form.Password("password",
-			form.notnull,
-			description = "Password",
-			class_ = "form-control",
-			placeholder='Enter employee password'),
-		form.Password('confirm_password',
-			description = "Confirm Password",
-			class_ = "form-control",
-			placeholder='Confirm employee password'),
-		form.Button("Register",
-			id='registerButton',
-			class_ = "btn btn-default btn-block"),
-    	validators = [form.Validator("Passwords didn't match.", lambda i: i.password == i.confirm_password)]
-		)
+    register_form = form.Form(
+        form.Textbox("first_name",
+            form.notnull,
+            description = "First Name",
+            class_ = "form-control",
+            placeholder='Enter employee first name'),
+        form.Textbox("last_name",
+            form.notnull,
+            description = "Last Name",
+            class_ = "form-control",
+            placeholder='Enter employee last name'),
+        form.Textbox("email",
+            form.notnull,
+            description = "Email",
+            class_ = "form-control",
+            placeholder='Enter employee email'),
+        form.Dropdown('privilege', 
+            [('0', 'Front Desk'), ('1', 'Cleaning Personnel')],
+            description = "Role",
+            class_ = "form-control",
+            placeholder='Select employee work position'),
+        form.Password("password",
+            form.notnull,
+            description = "Password",
+            class_ = "form-control",
+            placeholder='Enter employee password'),
+        form.Password('confirm_password',
+            description = "Confirm Password",
+            class_ = "form-control",
+            placeholder='Confirm employee password'),
+        form.Button("Register",
+            id='registerButton',
+            class_ = "btn btn-default btn-block"),
+        validators = [form.Validator("Passwords didn't match.", lambda i: i.password == i.confirm_password)]
+        )
 
-	nullform = form.Form()
+    nullform = form.Form()
 
-	def GET(self):
-		usr = ''
+    def GET(self):
+        usr = ''
 
-		if logged():
-			usr = 'placeholder'
-			# render = create_render(session.privilege)
-			# return render.register("HMS | Register", self.nullform, "", "", "Already logged in.")
-		else:
-			# session.login = 0
-			# session.privilege = -1
-			pass
-		
-		render = create_render(session.privilege)
-		return render.register("HMS | Register", self.register_form(), usr, "", "")
+        if logged():
+            usr = 'placeholder'
+            # render = create_render(session.privilege)
+            # return render.register("HMS | Register", self.nullform, "", "", "Already logged in.")
+        else:
+            # session.login = 0
+            # session.privilege = -1
+            pass
+        
+        render = create_render(session.privilege)
+        return render.register("HMS | Register", self.register_form(), usr, "", "")
 
-	def POST(self):
-		global user_ids
-		form = self.register_form()
-		msg = ""
-		err = ""
-		
-		if not form.validates():
-			session.login = 0
-			session.privilege = -1
-			err = "Invalid fields."
-		else:
+    def POST(self):
+        global user_ids
+        form = self.register_form()
+        msg = ""
+        err = ""
+        
+        if not form.validates():
+            session.login = 0
+            session.privilege = -1
+            err = "Invalid fields."
+        else:
 
-			cur.execute('SELECT * FROM Users WHERE email=%s', (form.d.email))
-			ident = fetchOneAssoc(cur)
+            cur.execute('SELECT * FROM Users WHERE email=%s', (form.d.email))
+            ident = fetchOneAssoc(cur)
 
-			try:
-				if form.d.email == ident['email']:
-					session.login = 0
-					session.privilege = -1
-					err = "User already registered."
-				else:
-					self.__helper(form)
-					msg = "User registered."
-			except:
-				self.__helper(form)
-				msg = "User registered."
+            try:
+                if form.d.email == ident['email']:
+                    session.login = 0
+                    session.privilege = -1
+                    err = "User already registered."
+                else:
+                    self.__helper(form)
+                    msg = "User registered."
+            except:
+                self.__helper(form)
+                msg = "User registered."
 
-		render = create_render(session.privilege)
-		return render.home('HMS | Home', session.name, '', msg, '')
+        render = create_render(session.privilege)
+        return render.home('HMS | Home', session.name, '', msg, '')
 
-	def __helper(self, form):
-		#SQL query to INSERT a record into the table FACTRESTTBL.
-		cur.execute('''INSERT INTO Users (first_name, last_name, encrypted_password, email, created_at, updated_at, current_sign_in_at, last_sign_in_at, current_sign_in_ip, last_sign_in_ip, privilege)
-						VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)''',
-						(form.d.first_name, 
-						form.d.last_name, 
-						hashlib.sha1(form.d.password).hexdigest(),
-						form.d.email,
-						self.__stamp(datetime.now()),
-						self.__stamp(datetime.now()),
-						self.__stamp(datetime.now()),
-						self.__stamp(datetime.now()),
-						web.ctx['ip'],
-						web.ctx['ip'],
-						form.d.privilege))
-		
-	    # Commit your changes in the database
-		db.commit()
+    def __helper(self, form):
+        #SQL query to INSERT a record into the table FACTRESTTBL.
+        cur.execute('''INSERT INTO Users (first_name, last_name, encrypted_password, email, created_at, updated_at, current_sign_in_at, last_sign_in_at, current_sign_in_ip, last_sign_in_ip, privilege)
+                        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)''',
+                        (form.d.first_name, 
+                        form.d.last_name, 
+                        hashlib.sha1(form.d.password).hexdigest(),
+                        form.d.email,
+                        self.__stamp(datetime.now()),
+                        self.__stamp(datetime.now()),
+                        self.__stamp(datetime.now()),
+                        self.__stamp(datetime.now()),
+                        web.ctx['ip'],
+                        web.ctx['ip'],
+                        form.d.privilege))
 
-	def __stamp(self, dt):
-	  # turns a python datetime object into a unix time stamp (seconds)
-	  return int(time.mktime( dt.timetuple() ))
+        # Commit your changes in the database
+        db.commit()
+
+    def __stamp(self, dt):
+      # turns a python datetime object into a unix time stamp (seconds)
+      return int(time.mktime( dt.timetuple() ))
 
 class Logout:
-	def GET(self):
-		session.kill()
-		raise web.seeother('/')
+    def GET(self):
+        session.kill()
+        raise web.seeother('/')
 
 class Home:
-	def GET(self):
-		render = create_render(session.privilege)
-		return render.home('HMS | Home', '', '', '', '')
+    def GET(self):
+        render = create_render(session.privilege)
+        return render.home('HMS | Home', '', '', '', '')
 
 class About:
-	def GET(self):
-		session.login = 0
-		session.privilege = -1
-		render = create_render(session.privilege)
-		return render.about('HMS | About', '')	
+    def GET(self):
+        session.login = 0
+        session.privilege = -1
+        render = create_render(session.privilege)
+        return render.about('HMS | About', '')  
 
 class Contact:
-	contact_form = form.Form(
-		form.Textbox('name',
-			form.notnull,
-			description="Name",
-			class_ = 'form-control',
-			placeholder='Enter your first and last name'),
-		form.Textbox('email',
-			form.notnull,
-			description='Email',
-			id='emailBox',
-			class_ = 'form-control',
-			placeholder='Enter your email'),
-		form.Textarea('comment',
-			form.notnull,
-			description='Questions or Comments',
-			id='passwordBox',
-			class_ = 'form-control',
-			style = 'height: 150px',
-			placeholder='Enter your questions or comments'),
-		form.Button('Submit',
-			id='submitButton',
-			class_ = 'btn btn-default btn-block'))
+    contact_form = form.Form(
+        form.Textbox('name',
+            form.notnull,
+            description="Name",
+            class_ = 'form-control',
+            placeholder='Enter your first and last name'),
+        form.Textbox('email',
+            form.notnull,
+            description='Email',
+            id='emailBox',
+            class_ = 'form-control',
+            placeholder='Enter your email'),
+        form.Textarea('comment',
+            form.notnull,
+            description='Questions or Comments',
+            id='passwordBox',
+            class_ = 'form-control',
+            style = 'height: 150px',
+            placeholder='Enter your questions or comments'),
+        form.Button('Submit',
+            id='submitButton',
+            class_ = 'btn btn-default btn-block'))
 
-	nullform = form.Form()
+    nullform = form.Form()
 
 
-	def GET(self):
-		form = self.contact_form()
-		msg = ''
-		err = ''
+    def GET(self):
+        form = self.contact_form()
+        msg = ''
+        err = ''
 
-		render = create_render(session.privilege)
-		return render.contact('HMS | Contact', '', form, msg, err)	
+        render = create_render(session.privilege)
+        return render.contact('HMS | Contact', '', form, msg, err)  
 
 def logged():
-	if session.login == 1:
-		return True
-	else:
-		return False
+    if session.login == 1:
+        return True
+    else:
+        return False
 
 def create_render(privilege):
-	if logged():
-		if privilege == 0:
-			render = web.template.render('templates/front_desk', base='../layout')
-		elif privilege == 1:
-			render = web.template.render('templates/cleaning_personnel', base='../layout')
-		elif privilege == 2:
-			render = web.template.render('templates/admin', base='../layout')
-		else:
-			render = web.template.render('templates/', base='layout')
-	else:
-		render = web.template.render('templates/', base='layout')
-	return render
+    if logged():
+        if privilege == 0:
+            render = web.template.render('templates/front_desk', base='../layout')
+        elif privilege == 1:
+            render = web.template.render('templates/cleaning_personnel', base='../layout')
+        elif privilege == 2:
+            render = web.template.render('templates/admin', base='../layout')
+        else:
+            render = web.template.render('templates/', base='layout')
+    else:
+        render = web.template.render('templates/', base='layout')
+    return render
 
-def fetchOneAssoc(cursor) :
+def fetchOneAssoc(cursor):
     data = cursor.fetchone()
     if data == None :
         return None
@@ -287,5 +296,20 @@ def fetchOneAssoc(cursor) :
 
     return dict
 
+def fetchAllAssoc(cursor):
+    data = cursor.fetchall()
+    if data == None:
+        return None
+
+    desc = cursor.description
+
+    dict = {}
+
+    for row in data:
+        for (name, value) in zip(desc, data):
+            dict[name[0]] = value
+
+    return dict
+
 if __name__ == "__main__":
-	app.run()
+    app.run()
